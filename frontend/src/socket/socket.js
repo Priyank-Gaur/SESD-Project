837e568b32fe55612cce53434a348c166606acba
gr.priyk <priyankgaur2005@gmail.com>
2026-04-19 01:24:05 +0530

    updated socket url to correctly adapt to production environment

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,4 +1,5 @@
 import {io} from 'socket.io-client';
-const SOCKET_URL=process.env.REACT_APP_SOCKET_URL||'http://localhost:3001';
+const apiUrl=process.env.REACT_APP_API_URL||'http://localhost:3001/api';
+const SOCKET_URL=process.env.REACT_APP_SOCKET_URL||apiUrl.replace('/api', '');
 const socket=io(SOCKET_URL, {autoConnect: true});
 export default socket;
